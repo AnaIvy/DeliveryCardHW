1cb7edb18bdcf03836a9070e51ed511fb2b85000
Papillon <anil_061293@gmail.com>
2023-05-08 23:37:38 +0300

    Add test

---
+++ src/test/java/ru/netology/SubmitFormTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+
+class SubmitFormTest {
+    @Test
+    void shouldSendFormToOrderDelivery() {
+        Configuration.holdBrowserOpen = true;
+        open("http://0.0.0.0:9999");
+        $("[data-test-id='city'] input").setValue("Чебоксары");
+        $("[pattern]").setValue("11.05.2023");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+71111111111");
+        $("[data-test-id='agreement'] span").click();
+        $(".button").click();
+        $x("//div [contains(text(),'Встреча успешно забронирована на')]").should(Condition.appear, Duration.ofSeconds(10));
+    }
+}
