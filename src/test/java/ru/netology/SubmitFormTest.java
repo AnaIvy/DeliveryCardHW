792fe4fa8bfe220f3697e9c6abc064554bc52d16
Papillon <anil_061293@gmail.com>
2023-05-13 10:43:28 +0300

    Corrective commit

--- src/test/java/ru/netology/SubmitFormTest.java
+++ src/test/java/ru/netology/SubmitFormTest.java
@@ -3,20 +3,36 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
 import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 
 class SubmitFormTest {
+    public String generateDate(int days) {
+        LocalDate LocalDate = null;
+        DateTimeFormatter DateTimeFormatter = null;
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String planningDate = generateDate(4);
+
     @Test
     void shouldSendFormToOrderDelivery() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Чебоксары");
-        $("[pattern]").setValue("11.05.2023");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+71111111111");
         $("[data-test-id='agreement'] span").click();
         $(".button").click();
-        $x("//div [contains(text(),'Встреча успешно забронирована на')]").should(Condition.appear, Duration.ofSeconds(12));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(12))
+                .shouldBe(Condition.visible);
     }
 }
